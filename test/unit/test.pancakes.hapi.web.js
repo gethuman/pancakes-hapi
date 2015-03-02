8bb336408b013210a26a6d842c17e0cd6b4602df
Jeff Whelpley <jeff@gethuman.com>
2015-03-02 12:57:57 -0500

    Updates for JWT

--- test/unit/test.pancakes.hapi.web.js
+++ test/unit/test.pancakes.hapi.web.js
@@ -12,7 +12,7 @@ describe('UNIT ' + name, function () {
     describe('addWebRoutes()', function () {
         it('should add 3 routes', function () {
             var routeSpy = taste.spy();
-            var opts = { server: { route: routeSpy } };
+            var opts = { server: { route: routeSpy }, config: {} };
             var context = { getWebRouteHandler: taste.spy() };
             hapi.addWebRoutes.call(context, opts);
             routeSpy.should.have.callCount(1);
